dfdf8b2a2008601d96038bfd4d9e1273fcc85b93
Yuki <github@risaton.net>
2024-09-01 19:08:56 +0900

    commit

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import {configDotenv} from "dotenv";
+import {Client, IntentsBitField, Events} from "discord.js";
+
+configDotenv()
+
+const TOKEN = process.env.DISCORD_TOKEN;
+
+const client = new Client({intents: [
+        IntentsBitField.Flags.Guilds
+    ]})
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+})
+
+client.login(TOKEN);
